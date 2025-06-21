58d16354f080565b11161ff06d2c0c913f010336
Raees Mughal <raees.mughal.official@gmail.com>
2025-06-21 16:04:16 +0500

    Update db.js

--- db.js
+++ db.js
@@ -8,7 +8,9 @@ const MONGODB_URI = process.env.MONGODB_URI;
 // async function we are gonna export and will be importing in out main file (server.js in this case)
 const connectDB = async ()=>{
     try{
+        console.log(MONGODB_URL + ' before')
         const conn = await mongoose.connect(MONGODB_URI);
+        console.log(MONGODB_URL + ' after')
         console.log('mongodb connected');
     }catch(er){
         console.log(er.message);
